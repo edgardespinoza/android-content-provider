37274d82e5996e38c25904eb286fb3f258632903
eespinor <edgard.espinoza.rivas@gmail.com>
2023-08-09 22:31:14 -0500

    first commit

---
+++ app/src/main/java/com/eespinor/contentproviders/ImageViewModel.kt
@@ -0,0 +1,17 @@
+package com.eespinor.contentproviders
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ImageViewModel : ViewModel() {
+
+    var images by mutableStateOf(emptyList<Image>())
+        private set
+
+    fun updateImages(images: List<Image>){
+        this.images = images
+    }
+
+}
